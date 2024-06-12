93067eb1e42b13671d19806a9b0a82e8fc2215a1
Jack Waller <jack.waller94@gmail.com>
2024-06-12 10:50:29 +1000

    chore: mock env for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default defineConfig({
 	test: {
 		include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		setupFiles: ['./vitest.setup.ts'],
 	},
 	plugins: [commonjs()],
 });
